8caaa691ed6bd67126c7d398428173458b4e393b
ApprenticeMGL <apprenticemn@gmail.com>
2021-04-29 20:54:31 +0800

    update

--- profile.php
+++ profile.php
@@ -59,15 +59,15 @@ if(!isset($_SESSION['username'])) {
                         <a href="mailto:<?php echo $row['email']; ?>" class="btn btn-primary"><?php echo $row['email']; ?></a>
                     </div>
                 </div>
-
+<hr>
                 <form action="/edit.php" method="POST">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Username</label>
-                        <input type="email" value="<?php echo $row['username']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="text" name="username" value="<?php echo $row['username']; ?>" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Name</label>
-                        <input type="text" value="<?php echo $row['name']; ?>" class="form-control" id="exampleInputPassword1">
+                        <input type="text" name="name" value="<?php echo $row['name']; ?>" class="form-control" id="exampleInputPassword1">
                     </div>
                     <button type="submit" class="btn btn-primary">Edit</button>
                 </form>
